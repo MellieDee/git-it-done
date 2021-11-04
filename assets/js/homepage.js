654b3aff49b214026d1b1a213b46a9f981ff5548
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2021-11-03 17:39:39 -0700

    link two html pages and error proof for incorrect or missing data by redirect to homepage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -67,9 +67,11 @@ var displayRepos = function(repos, searchTerm) {
   for (var i =0; i <repos.length; i++) {
     //format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
-    //createa container for each repo
-    var repoEl = document.createElement("div");
+
+    //create a container for each repo
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     //create span el to hold repo name
     var titleEl =  document.createElement("span");
--- assets/js/single.js
+++ assets/js/single.js
@@ -1,25 +1,61 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var linkEl = document.createElement("a");
 
-var getRepoIssues = function(repo) {
-  
+var repoNameEl = document.querySelector("#repo-name");
+
+
+getRepoName = function () {
+  //grab name from query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  // console.log(repoName);
+  if (repoName) {
+    //if the repo name exiss ie is TRUE then, display repo Name on the page
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    //if not repo was given redirect to the home page
+    document.location.replace("./index.html");
+  }
+};
+
+var displayWarning = function (repo) {
+  limitWarningEl.textContent = "To see more than 30 issues, visit "
+  linkEl.textContent = "See more Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+  //append to warning container
+  limitWarningEl.appendChild(linkEl);
+  console.log(repo);
+};
+
+var getRepoIssues = function (repo) {
   //endpoint to hit
-
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
   //now create http request to hit the endpoint using fetch API f
 
+  //make a GET request to the URL
   fetch(apiUrl).then(function(response) {
-    if(response.ok) {
-      response.json().then(function(data) {
-      console.log(data);
-      displayIssues(data); 
+    //.if request was successful then display the data
+    if (response.ok) {
+      response.json().then(function (data) {
+        // console.log(data);
+        displayIssues(data);
+
+        //check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
-      alert("There was a probblem wiht your request!");
+      //if not successful diret o homepage
+      document.location.replace("./index.html");
     }
   });
 };
 
-var displayIssues = function(issues) {
+var displayIssues = function (issues) {
   if (issues.length === 0) {
     issueContainerEl.textContent = "This repo has no open issues!";
   }
@@ -33,22 +69,22 @@ var displayIssues = function(issues) {
     // Again, this looks like what we did with repo data in homepage.js. The biggest difference is the data we're working with. Issue objects have an html_url property, which links to the full issue on GitHub. We also added a target="_blank" attribute to each <a> element, to open the link in a new tab instead of replacing the current webpage.
 
     issueContainerEl.appendChild(issueEl);
-  
 
-  //create span to hold issue title
-  var titleEl = document.createElement("span");
-  titleEl.textContent = issues[i].title;
 
-  //append to container
-  issueEl.appendChild(titleEl);
+    //create span to hold issue title
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
 
-  //create a type element
-  var typeEl = document.createElement("span");
+    //append to container
+    issueEl.appendChild(titleEl);
 
-  //check if issue is issue or pull req
-  if (issues[i].pull_request) {
-    typeEl.textContent = "(Pull request)";
-   } else {
+    //create a type element
+    var typeEl = document.createElement("span");
+
+    //check if issue is issue or pull req
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull request)";
+    } else {
       typeEl.textContent = "(Issue)";
     }
     //append to container
@@ -56,4 +92,5 @@ var displayIssues = function(issues) {
   };
 };
 
-getRepoIssues("melliedee/portfolio");
+getRepoName()
+// getRepoIssues("facebook/react");
